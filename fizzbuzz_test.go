494b571259ebc55ac18a44bc09717c8d14150068
PrakitDesktop <boatnakrub_@hotmail.com>
2024-05-19 17:07:08 +0700

    should return 2 when input 2

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -13,4 +13,15 @@ func TestFizzBuzz(t *testing.T) {
 			t.Errorf("want %v but got %v", want, got)
 		}
 	})
+
+	t.Run("should return 2 when input 2", func(t *testing.T) {
+		input := 2
+
+		got := FizzBuzz(input)
+		want := "2"
+
+		if got != want {
+			t.Errorf("want %v but got %v", want, got)
+		}
+	})
 }

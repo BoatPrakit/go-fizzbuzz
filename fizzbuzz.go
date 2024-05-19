5439dd07b9cb7cb8940be316cafd44fd514f8c2d
PrakitDesktop <boatnakrub_@hotmail.com>
2024-05-19 18:06:36 +0700

    refactor to uise only one if

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -3,17 +3,23 @@ package fizzbuzz
 import "strconv"
 
 func FizzBuzz(input int) string {
-	if input == 15 {
-		return "FizzBuzz"
-	}
-
-	if input%3 == 0 {
-		return "Fizz"
-	}
-
-	if input%5 == 0 {
-		return "Buzz"
+	if input%3 == 0 || input%5 == 0 {
+		return IsFizzBuzz(input)
 	}
 
 	return strconv.Itoa(input)
+}
+
+func IsFizzBuzz(n int) string {
+	isFizz := map[bool]string{
+		true:  "Fizz",
+		false: "",
+	}
+
+	isBuzz := map[bool]string{
+		true:  "Buzz",
+		false: "",
+	}
+
+	return isFizz[n%3 == 0] + isBuzz[n%5 == 0]
 }

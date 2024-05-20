62dd5495f5064cec080adafff9f73d4ed37afb28
prakit.w <prakit.w@arise.tech>
2024-05-20 10:04:27 +0700

    should return Buzz when input 5

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -3,8 +3,24 @@ package fizzbuzz
 import "strconv"
 
 func FizzBuzz(input int) string {
-	if input == 3 {
-		return "Fizz"
+	if input == 3 || input == 5 {
+		return Fizz(input) + Buzz(input)
 	}
 	return strconv.Itoa(input)
+}
+
+func Fizz(input int) string {
+	fizz := map[bool]string{
+		true:  "Fizz",
+		false: "",
+	}
+	return fizz[input == 3]
+}
+
+func Buzz(input int) string {
+	buzz := map[bool]string{
+		true:  "Buzz",
+		false: "",
+	}
+	return buzz[input == 5]
 }

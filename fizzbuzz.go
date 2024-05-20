935a5227d92fa8f4c8c6a77c3edacef224d70ebc
prakit.w <prakit.w@arise.tech>
2024-05-20 09:42:58 +0700

    should return Fizz when input 3

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -4,5 +4,10 @@ func FizzBuzz(input int) string {
 	if input == 2 {
 		return "2"
 	}
+
+	if input == 3 {
+		return "Fizz"
+	}
+
 	return "1"
 }
--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -13,7 +13,7 @@ func TestFizzBuzz(t *testing.T) {
 	}{
 		{1, "1"},
 		{2, "2"},
-		// {3, "Fizz"},
+		{3, "Fizz"},
 		// {4, "4"},
 		// {5, "Buzz"},
 		// {6, "Fizz"},

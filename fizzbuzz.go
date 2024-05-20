62119b98b188cb2585ebbcbe276f42ebbea6be5d
prakit.w <prakit.w@arise.tech>
2024-05-20 09:49:32 +0700

    should return FizzBuzz when input 15

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -3,13 +3,25 @@ package fizzbuzz
 import "fmt"
 
 func FizzBuzz(input int) string {
-	if input%3 == 0 {
+	if isFizz(input) && isBuzz(input) {
+		return "FizzBuzz"
+	}
+
+	if isFizz(input) {
 		return "Fizz"
 	}
 
-	if input == 5 || input == 10 {
+	if isBuzz(input) {
 		return "Buzz"
 	}
 
 	return fmt.Sprintf("%v", input)
+}
+
+func isFizz(n int) bool {
+	return n%3 == 0
+}
+
+func isBuzz(n int) bool {
+	return n%5 == 0
 }

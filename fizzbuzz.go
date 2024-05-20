06c9d8e96ab0c04acd419dbaa16c8124d2dfdd3a
prakit.w <prakit.w@arise.tech>
2024-05-20 09:41:52 +0700

    start round 2: should return 1 when input is 1

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -1,26 +1,5 @@
 package fizzbuzz
 
-import "strconv"
-
 func FizzBuzz(input int) string {
-	return IsFizzBuzz(input)
-}
-
-func IsFizzBuzz(n int) string {
-	isFizz := map[bool]string{
-		true:  "Fizz",
-		false: "",
-	}
-
-	isBuzz := map[bool]string{
-		true:  "Buzz",
-		false: "",
-	}
-
-	isNormal := map[bool]string{
-		true:  strconv.Itoa(n),
-		false: "",
-	}
-
-	return isFizz[n%3 == 0] + isBuzz[n%5 == 0] + isNormal[n%3 != 0 && n%5 != 0]
+	return "1"
 }

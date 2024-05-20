8169f2ceec568502a4062ab84c6320ecc55c678a
prakit.w <prakit.w@arise.tech>
2024-05-20 09:52:53 +0700

    should return Fizz when input 3

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -1,8 +1,10 @@
 package fizzbuzz
 
+import "strconv"
+
 func FizzBuzz(input int) string {
-	if input == 2 {
-		return "2"
+	if input == 3 {
+		return "Fizz"
 	}
-	return "1"
+	return strconv.Itoa(input)
 }
